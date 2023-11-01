f9b130eb729e92b0878ff77829ea736bd7485880
Thor Lund Hansen <nexus.gubbih@gmail.com>
2023-11-01 15:48:39 +0100

    Prettier

--- Src/models/user.ts
+++ Src/models/user.ts
@@ -1,8 +1,8 @@
 export interface user {
-    id?: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password?: string;
-    orgId: number[];
-  }
+  id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password?: string;
+  orgId: number[];
+}
