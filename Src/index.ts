1bda0ff6fa55a9246269db39a9f4e7fe059b8aef
Thor Lund Hansen <nexus.gubbih@gmail.com>
2023-11-02 15:09:04 +0100

    Register user with same email [sc-102]

--- Src/index.ts
+++ Src/index.ts
@@ -2,7 +2,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import eventRoutes from './routes/events';
 import userRoutes from './routes/users';
 import cors from 'cors';
-import { errorLogger, errorResponder, invalidPathHandler } from './errorHandler/handlerForExpress';
+import {
+  errorLogger,
+  errorResponder,
+  invalidPathHandler,
+} from './errorHandler/handlerForExpress';
 
 export const app = express();
 const port = process.env.PORT || 3000;
@@ -14,20 +18,22 @@ app.use('/events', eventRoutes);
 app.use('/users', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express! Go to <a href="https://localhost:3010/">localhost:3010/</a> to see the app');
+  res.send(
+    'Hello, TypeScript Express! Go to <a href="https://localhost:3010/">localhost:3010/</a> to see the app',
+  );
 });
 
 // Attach the first Error handling Middleware
 // function defined above (which logs the error)
-app.use(errorLogger)
+app.use(errorLogger);
 
 // Attach the second Error handling Middleware
 // function defined above (which sends back the response)
-app.use(errorResponder)
+app.use(errorResponder);
 
 // Attach the fallback Middleware
 // function which sends back the response for invalid paths)
-app.use(invalidPathHandler)
+app.use(invalidPathHandler);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
