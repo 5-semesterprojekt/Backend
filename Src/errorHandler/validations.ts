3274d9053b57005d3f5d67586539a10ef2068cbf
Thor Lund Hansen <90314333+gubbih@users.noreply.github.com>
2023-11-08 12:06:53 +0100

    Update Src/errorHandler/validations.ts
    Co-authored-by: Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>

--- Src/errorHandler/validations.ts
+++ Src/errorHandler/validations.ts
@@ -17,7 +17,7 @@ export function isValidUser(user: user): boolean {
 
 // Function to check if a given string is a valid email address
 function isValidEmail(email: string): boolean {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(email);
 }
 
