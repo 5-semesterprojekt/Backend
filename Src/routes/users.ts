27602cc6caa32ea0ca6196ad97f765c3422b908f
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-11-15 10:58:23 +0100

    FAIL

--- Src/routes/users.ts
+++ Src/routes/users.ts
@@ -47,8 +47,7 @@ router.post(
 router.post(
   `/:orgId/login`,
   userLoginValidationRules,
-  asyncHandler(async (req: Request, res: Response) => {
-    const user: { user: User } = await userLogin(
+  asyncHandler(async (req: Request, res: Response) => {const user: { user: User } = await userLogin(
       req.body.email,
       req.body.password,
       req.params.orgId,
