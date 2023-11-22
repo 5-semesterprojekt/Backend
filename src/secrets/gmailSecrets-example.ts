0b01971ccebe2cd39c26aa6f89fe7b15936dad60
Thor Lund Hansen <nexus.gubbih@gmail.com>
2023-11-22 11:28:25 +0100

    made a comment

--- src/secrets/gmailSecrets-example.ts
+++ src/secrets/gmailSecrets-example.ts
@@ -1,2 +1,4 @@
 export const gmail = 'Gmail account';
 export const gmailPassword = 'Password to the gmail account';
+// go to https://myaccount.google.com/u/1/signinoptions/two-step-verification
+// and scroll down to app password and generate a password for the app
